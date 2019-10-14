d9f6b8909451a8a9459d1ef65ba9dfe9e023d16b
Tarcle <451575@naver.com>
2019-10-14 18:31:39 +0900

    fetch -> XMLHttpRequest, progressbar

--- compare.js
+++ compare.js
@@ -2,74 +2,82 @@ var app = new Vue({
     el: 'app',
     template: '#template',
     data: {
-        load_page: 15,
+        load_page: 15, //불러올 페이지 수 (한 페이지 8곡)
         
-        opened: true,
-        url1: 'http://scoresaber.com/u/76561198053868259',
-        url2: 'http://scoresaber.com/u/76561198298645747',
-        player1name: '플레이어 1',
-        player2name: '플레이어 2',
-        name: [],
-        player1data: [],
-        player2data: [],
+        opened: true, //검색칸 토글
+        url1: 'http://scoresaber.com/u/76561198053868259', //왼쪽 주소
+        url2: 'http://scoresaber.com/u/76561198298645747', //오른쪽 주소
+        player1name: '플레이어 1', //왼쪽 이름
+        player2name: '플레이어 2', //오른쪽 이름
+        player1data: [], //왼쪽 데이터
+        player2data: [], //오른쪽 데이터
+        load_count: 0, //불러온 페이지 개수
     },
     methods: {
-        compare() {
+        compare: function () {
             this.player1data = [];
             this.player2data = [];
             this.getSongs();
         },
-        getSongs() {
+        getSongs: function () {
             headers = {
                 'origin': '1',
                 'x-requested-with':'1',
             };
-            proms = [];
-            need = [];
-            function fetch_(url, playerdata, player) {
-                need[player] = true;
-                for(i=1; i<=app.load_page; i++) {
-                    url += '&page='+i+'&sort=1'
-                    proms.push(fetch('http://cors-anywhere.herokuapp.com/'+url, {headers: headers, data: {i: i}})
-                    .then(response => response.text())
-                    .then(text => {
-                        html = document.createElement('div');
-                        html.innerHTML = text;
-                        if(need[player]) {
-                            app.name[player] = html.querySelector('.title a').innerText;
-                            need[player] = false;
-                        }
-                        songs = html.querySelectorAll("table.ranking.songs>tbody>tr");
-                        for(j=0; j<songs.length; j++) {
-                            song = songs[j];
-                            name_html = song.querySelector("th.song a").innerHTML;
-                            pp = song.querySelector("th.score .ppValue").innerText;
-                            pp_html = song.querySelector("th.score").innerHTML;
-                            data = {name_html: name_html, pp: parseFloat(pp), pp_html: pp_html};
-                            playerdata.push(data);
-                        }
-                        return playerdata;
-                    }));
-                }
+            app.name = []; //렌더링 전 이름 저장
+            app.result = [[], []]; //렌더링 전 결과 저장
+
+            app.load_count = 0;
+            for(i=1; i<=app.load_page; i++) {
+                loadData('http://cors-anywhere.herokuapp.com/'+this.url1+'&page='+i+'&sort=1', 0);
+                loadData('http://cors-anywhere.herokuapp.com/'+this.url2+'&page='+i+'&sort=1', 1);
             }
-            result1 = [];
-            result2 = [];
-            fetch_(this.url1, result1, 1);
-            fetch_(this.url2, result2, 2);
-            Promise.all(proms).then(() => {
-                app.player1name = app.name[1];
-                app.player2name = app.name[2];
-                result1.sort((a,b) => b.pp - a.pp);
-                result2.sort((a,b) => b.pp - a.pp);
-                app.player1data = result1;
-                app.player2data = result2;
-            })
+
+            function loadData(url, player) {
+                var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+                xhr.onreadystatechange = function () {
+                    if(this.readyState == 4) {
+                        if(this.status == 200) {
+                            text = this.response.replace(/<IMG(.*?)>/gi, "");
+                            html = document.createElement('div');
+                            html.innerHTML = text;
+                            if(!app.name[player]) app.name[player] = html.querySelector('.title').innerText.trim();
+                            songs = html.querySelectorAll("table.ranking.songs>tbody>tr");
+                            for(i=0; i<songs.length; i++) {
+                                song = songs[i];
+                                name_html = song.querySelector("th.song a").innerHTML;
+                                pp = song.querySelector("th.score .ppValue").innerText;
+                                pp_html = song.querySelector("th.score").innerHTML;
+                                data = {name_html: name_html.trim(), pp: parseFloat(pp), pp_html: pp_html.trim()};
+                                // app.result[player].push(data);
+                                app['player'+(player+1)+'data'].push(data);
+                            }
+                            if(++app.load_count >= app.load_page*2) {
+                                app.player1name = app.name.shift();
+                                app.player2name = app.name.shift();
+                                app.player1data.sort((a,b) => b.pp - a.pp);
+                                app.player2data.sort((a,b) => b.pp - a.pp);
+                                // app.player1data = app.result.shift();
+                                // app.player2data = app.result.shift();
+                            }
+                        } else alert("데이터 로드에 실패했습니다.");
+                    }
+                }
+                xhr.open("GET", url, true);
+                xhr.setRequestHeader('x-requested-with', 'XMLHttpRequest');
+                xhr.send();
+            }
         },
-        player2pp(data) {
+        player2pp: function (data) {
             return this.player2data.filter(data2 => data.name_html == data2.name_html)[0];
         },
-        getWinner(data) {
+        getWinner: function (data) {
             return data.pp >= ((tmp=this.player2pp(data))?tmp.pp:0) ? 1 : 2
+        },
+    },
+    computed: {
+        progress: function () {
+            return this.load_count/this.load_page/2 * 100;
         }
     }
 })
